280ecd71474e5705502382b7c090064fe55a397e
Steven Lombardi <steve.lombardi@connexta.com>
2021-02-10 14:09:22 -0700

    Simplify application-init macro & ns gen

--- deps-draw-graph/src/main/clojure/com/connexta/osgeyes/main.clj
+++ deps-draw-graph/src/main/clojure/com/connexta/osgeyes/main.clj
@@ -49,12 +49,13 @@
   'com.connexta.osgeyes.cmds' are pulled in so they needn't be qualified for use. Any immediate
   child directories of 'user.dir' are bound to the 'osgeyes' ns as symbols for use in the CLI
   along with autocomplete."
-  `(do
-     ~'(ns osgeyes
+  (concat
+    (list 'do
+      '(ns osgeyes
          (:require [com.connexta.osgeyes.env :as env])
          (:use [com.connexta.osgeyes.cmds]))
-     ~`(def ~'help "Displays the help text." (fn [] ~application-help))
-     ~@(map #(list 'def (symbol %) %) (env/list-subdirs))))
+      (list 'defn 'help "Displays the help text." [] application-help))
+    (map #(list 'def (symbol %) %) (env/list-subdirs))))
 
 (defn -main
   "Entry point for the customized REPLy instance."
@@ -74,30 +75,3 @@
       (finally
         (index/close-indexer!)
         (shutdown-agents)))))
-
-;; ----------------------------------------------------------------------
-;; # Commentary
-;;
-
-(comment
-  application-init
-  "You might think that the below should work for an appropriate application-init value, but
-  it doesn't. Why not? Hint: (def ...) is not what you think it is."
-  (do
-    (ns osgeyes
-      (:require [com.connexta.osgeyes.env :as env])
-      (:use [com.connexta.osgeyes.cmds]))
-    (map #(def (symbol %) %) (env/list-subdirs))))
-
-(comment
-  "The result of the first eval of the application-init data structure defined above. This is
-  actually what the code looks like during the 2nd evaluation when REPLy loads it."
-  (do
-    (ns osgeyes
-      (:require [com.connexta.osgeyes.env :as env])
-      (:use [com.connexta.osgeyes.cmds]))
-    (def subdir1 "subdir1")
-    (def subdir2 "subdir2")
-    (def subdir3 "subdir3")
-    ;; ...
-    (def subdirN "subdirN")))
